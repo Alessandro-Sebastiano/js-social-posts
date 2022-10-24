e659b55f5b91741df0ca5f4110c6042b8c517b52
Alessandro-Sebastiano <alesebastiano15@gmail.com>
2022-10-24 16:06:12 +0200

    posts

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const primaryContainer = document.getElementById('container');
 
 const posts = [
     {
@@ -60,6 +61,61 @@ const posts = [
 ];
 
 
+
+
+
+
+function generatePosts() {
+    for (let element of posts) {
+        const postsBox = document.createElement('div');
+
+        postsBox.innerHTML = `
+        <div class="post">
+            <div class="post__header">
+                <div class="post-meta">
+                    <div class="post-meta__icon">
+                        <img class="profile-pic" src="${element.author.image}" alt="${element.author.name}">
+                    </div>
+                    <div class="post-meta__data">
+                        <div class="post-meta__author">${element.author.name}</div>
+                        <div class="post-meta__time">${element.created}</div>
+                    </div>
+                </div>
+            </div>
+            <div class="post__text">${element.content}</div>
+            <div class="post__image">
+                <img src="${element.media}" alt="">
+            </div>
+            <div class="post__footer">
+                <div class="likes js-likes">
+                    <div class="likes__cta">
+                        <a class="like-button  js-like-button" href="#" data-postid="1">
+                            <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                            <span class="like-button__label">Mi Piace</span>
+                        </a>
+                    </div>
+                    <div class="likes__counter">
+                        Piace a <b id="like-counter-1" class="js-likes-counter">${element.likes}</b> persone
+                    </div>
+                </div>
+            </div>
+        </div>
+        `
+        primaryContainer.appendChild(postsBox);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+generatePosts();
 
 
 
